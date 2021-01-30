0aace362dab315177df11f9fd686bee11ab294f8
valeriasGitHub <77691053+valeriasGitHub@users.noreply.github.com>
2021-01-29 20:03:52 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// Import Article
+import Article from './Article.js';
+
+// Pass the values to the constructor method of Article
+const a = new Article (
+  {
+    title: "This is the title!",
+    author: "Me",
+    text: "This is an example article!"
+  }
+);
+
+// Shows its HTML on the console
+console.log(a.render() );
+
+class ArticleTitle {
+
+  constructor(title) {
+    this.props = {};
+    this.props.title = title;
+    
+  }
+
+  render() {
+    return `<h1>${this.props.title}</h1>`;
+  }
+
+}
