462dddf6c5eed0cf731a37ccd721c1204161ef3e
valeriasGitHub <vnavarro@knights.ucf.edu>
2021-03-10 19:49:09 -0500

    potfolio 1

--- week7/src/App.js
+++ week7/src/App.js
@@ -1,18 +1,37 @@
 
+import { Component } from 'react';
 import './App.css';
 import Laps from './components/Laps';
 import Situps from './components/Situps';
+import Tips from './components/Tips';
 
-function App() {
+class App extends Component {
+    state = {
+    visible : true
+  };
+
+  handleHide= () => {
+    this.setState({
+      visible: false
+    })
+  }
+  render() {
   return (
-    <div>
+    <div className="App">
+      {this.state.visible ? <Tips/> : null }
+      <button 
+      onClick={this.handleHide}>
+        Hide Tips
+      </button>
       <h1>Exercise Tracker:</h1>
       <Laps/>
       <Situps/>
+      
     </div>
+    
     
   );
 }
-
+}
 
 export default App;
---
+++ week7/src/components/Tips.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Tips extends React.Component {
+    render () {
+        return <h2 style= {{
+            fontsize: "30px",
+            color: "#45A557",
+            fontFamily: "fantasy",
+        }}>Healthy Tips: Drink water and stretch before exercising!!</h2>
+    }
+}
+export default Tips;
